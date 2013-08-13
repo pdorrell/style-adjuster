8ee6af2bad3ad02671f1e3ddbb68034073e10de3
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-08-13 14:53:27 +1200

    extension works! (mostly)

--- Rakefile
+++ Rakefile
@@ -2,15 +2,17 @@
 STDOUT.sync = true
 BASE_DIR = File.dirname(__FILE__)
 
+EXTENSION_BUILD_DIR = File.join(BASE_DIR, "build/extension")
+
 IMAGE_CSS_FILES = ["js/jquery-ui-1.10.3/themes/base/jquery-ui.css"]
 
-task :default => [:chrome_extension_css]
+task :default => [:chrome_extension]
 
 CHROME_EXTENSION_URL_PREFIX = "chrome-extension://__MSG_@@extension_id__/"
 
 task :chrome_extension_css do
   for css_file in IMAGE_CSS_FILES do
-    css_file_relative_dir = File.dirname(css_file)
+    css_file_relative_dir = 'base'
     puts "css_file_relative_dir = #{css_file_relative_dir}"
     css_file_name = File.join(BASE_DIR, css_file)
     puts "Reading CSS file #{css_file_name} ..."
@@ -27,4 +29,27 @@ task :chrome_extension_css do
     puts " wrote Chrome extension CSS to #{css_out_file_name}"
   end
 end
-  
+
+directory EXTENSION_BUILD_DIR
+
+EXTENSION_SRC_FILES = Dir.chdir(BASE_DIR) do
+  Dir.glob %w' manifest.json
+               start-style-adjuster.js
+               load-style-adjuster-for-extension.js
+               lib/style-adjuster.js
+               lib/style-adjuster.css
+               lib/help.html
+               js/jquery-1.10.2.js
+               js/jquery-ui-1.10.3/jquery-ui.js
+               js/jquery-ui-1.10.3/themes/base
+             '
+end
+
+task :chrome_extension => [EXTENSION_BUILD_DIR, :chrome_extension_css] do
+  FileUtils.cp_r EXTENSION_SRC_FILES, EXTENSION_BUILD_DIR, :verbose => true
+end
+
+task :clean do
+  FileUtils.rm_r EXTENSION_BUILD_DIR, :verbose => true
+end
+
--- load-style-adjuster-for-extension.js
+++ load-style-adjuster-for-extension.js
@@ -2,33 +2,16 @@
 chrome.browserAction.onClicked.addListener(function(tab) {
   // No tabs or host permissions needed!
   console.log('Starting Style Adjuster');
-  chrome.tabs.insertCSS({ file: 'js/jquery-ui-1.10.3/themes/base/jquery-ui.extension.css'}); 
-  chrome.tabs.insertCSS({ file: 'lib/style-adjuster.css'}); 
-  chrome.tabs.executeScript({ file: 'js/jquery-1.10.2.js'}); 
-  chrome.tabs.executeScript({ file: 'js/jquery-ui-1.10.3/jquery-ui.js'}); 
-  chrome.tabs.executeScript({ file: 'lib/style-adjuster.js'}); 
-  chrome.tabs.executeScript({ code: 'startStyleAdjuster();'}); 
+  function insertCssFile(file) {
+    console.log("Inserting CSS " + file + " ...");
+    chrome.tabs.insertCSS({ file: file});
+  }
+    
+  insertCssFile('base/jquery-ui.extension.css');
+  insertCssFile('style-adjuster.css');
+  chrome.tabs.executeScript({ file: 'jquery-1.10.2.js'}); 
+  chrome.tabs.executeScript({ file: 'jquery-ui.js'}); 
+  chrome.tabs.executeScript({ file: 'style-adjuster.js'}); 
+  chrome.tabs.executeScript({ file: 'start-style-adjuster.js'}); 
 });
-
-function startStyleAdjuster() {
-
-  // Create the Style Adjuster model from the document's style sheets
-  var styleAdjusterModel = new STYLE_ADJUSTER.StyleAdjusterModel(document.styleSheets);
-  
-  // initially deselect style sheets from jquery UI styles
-  styleAdjusterModel.deselectStyleSheets(function (styleSheet) {
-    var href = styleSheet.href;
-    return href == null || href.indexOf("jquery") != -1;
-  });
-  
-  // Create the Style Adjuster dialog (and display it)
-  var styleAdjusterView = new STYLE_ADJUSTER.StyleAdjusterView($("body"), styleAdjusterModel);
-  
-  // Set Ctrl-Alt-F12 to hide/show the dialog
-  $("body").keydown(function(event, ui) {
-    if (event.which == 123 && !event.shiftKey && event.altKey && event.ctrlKey) { // Ctrl-Alt-F12
-      styleAdjusterView.toggle();
-    }
-  });
-}();
 
