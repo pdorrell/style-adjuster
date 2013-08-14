4b5f1845dcf48f40ec5a7e6f36778b54ca39de0b
Philip Dorrell <philip@Cangallo-Ubuntu.(none)>
2013-08-14 16:22:25 +1200

    remove jquery dependency from index.html (only required by popup)

--- load-style-adjuster.js
+++ load-style-adjuster.js
@@ -1,21 +1,8 @@
-$(document).ready(function(){
+function loadStyleAdjuster() {
 
   // Create the Style Adjuster model from the document's style sheets
   var styleAdjusterModel = new STYLE_ADJUSTER.StyleAdjusterModel(document.styleSheets);
   
-  // initially deselect style sheets from jquery UI styles
-  styleAdjusterModel.deselectStyleSheets(function (styleSheet) {
-    var href = styleSheet.href;
-    return href == null || href.indexOf("jquery") != -1;
-  });
-  
   // Create the Style Adjuster dialog (and display it)
-  var styleAdjusterView = new STYLE_ADJUSTER.StyleAdjusterView($("body"), styleAdjusterModel);
-  
-  // Set Ctrl-Alt-F12 to hide/show the dialog
-  $("body").keydown(function(event, ui) {
-    if (event.which == 123 && !event.shiftKey && event.altKey && event.ctrlKey) { // Ctrl-Alt-F12
-      styleAdjusterView.toggle();
-    }
-  });
-});
+  var styleAdjusterView = new STYLE_ADJUSTER.StyleAdjusterView(null, styleAdjusterModel);
+};
